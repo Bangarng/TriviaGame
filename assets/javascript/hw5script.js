280f3a83cf3e5a210c5f0a279dece1bd0e762cd6
Dickie Stevens <rsk0528@gmail.com>
2018-06-15 17:20:44 -0400

    Trivia Game

--- assets/javascript/hw5script.js
+++ assets/javascript/hw5script.js
@@ -3,6 +3,7 @@ var wrong = 0;
 
 var BTMTrivia = {
   question1: {
+    name: "question1",
     header: "What does BTM stand for?",
     correctAnswer: "Boloney Time Machine",
     answer1: "Bogus Tree Man",
@@ -10,6 +11,7 @@ var BTMTrivia = {
     answer3: "Big Troll Mountain",
   },
   question2: {
+    name: "question2",
     header: "Who created BTM?",
     correctAnswer: "Dickie Stevens",
     answer1: "Stan Lee",
@@ -17,6 +19,7 @@ var BTMTrivia = {
     answer3: "Oprah Winfrey",
   },
   question3: {
+    name: "question3",
     header: "What color is Hero's costume and hair?",
     correctAnswer: "Blue and Yellow",
     answer1: "purple and white",
@@ -24,6 +27,7 @@ var BTMTrivia = {
     answer3: "pink and magenta",
   },
   question4: {
+    name: "question4",
     header: "What is the superhero society in BTM?",
     correctAnswer: "H.A.V.V.O.C.",
     answer1: "P.L.A.Y.E.R.S.",
@@ -31,6 +35,7 @@ var BTMTrivia = {
     answer3: "What society?",
   },
   question5: {
+    name: "question5",
     header: "Whose weapon of choice is a grappling hook?",
     correctAnswer: "Sidekick",
     answer1: "Shadowtaken",
@@ -38,6 +43,7 @@ var BTMTrivia = {
     answer3: "Mothman",
   },
   question6: {
+    name: "question6",
     header: "Who is the villain?",
     correctAnswer: "There isn't one yet",
     answer1: "Lizard Man",
@@ -45,6 +51,7 @@ var BTMTrivia = {
     answer3: "That bouncer from Jerry Springer",
   },
   question7: {
+    name: "question7",
     header: "What is the URL for Boloney Time Machines website?",
     correctAnswer: "boloneytimemachine.com",
     answer1: "google.com",
@@ -52,6 +59,7 @@ var BTMTrivia = {
     answer3: "imgur.com",
   },
   question8: {
+    name: "question8",
     header: "What kind of music is usally on BTM's 'The Funk' Page?",
     correctAnswer: "EDM",
     answer1: "Country",
@@ -59,6 +67,7 @@ var BTMTrivia = {
     answer3: "Rap",
   },
   question9: {
+    name: "question9",
     header: "What is Hero and Sidekicks favorite food?",
     correctAnswer: "Hot Dogs",
     answer1: "Pizza",
@@ -66,6 +75,7 @@ var BTMTrivia = {
     answer3: "Spinach",
   },
   question10: {
+    name: "question10",
     header: "What does Hero hate most?",
     correctAnswer: "Mustard",
     answer1: "Snakes",
@@ -101,23 +111,35 @@ function decrement() {
 function stop() {
   clearInterval(intervalId);
 }
-
+/*
+  <div class="">
+          <h1>What the heck?</h1>
+          <label><input type="radio" class="radio-inline" value="">Neopolitan</label>
+          <label><input type="radio"class="radio-inline" name="radios" value="">Mint Choco Chip</label>
+          <label><input type="radio" class="radio-inline" name="radios" value="">Blue Moon</label>
+            <label><input type="radio" class="radio-inline" name="radios" value="">All the Ice Cream</label>
+  </div>
+*/            
 function TrivaQuestions(object) {
   for (var p in object) {
     if(object.hasOwnProperty(p)) {
-      var questionH1 = $("<h1 class='question'>" + object[p].header + "</h1>");
+      var formGroup = $("<div>");
+      var questionH1 = $(formGroup).append("<h1 class='question'>" + object[p].header + "</h1>");
       $(".questionaire").append(questionH1);
-      var answer1 = $("<div><input type='radio' data-answer=false value=" + object[p].answer1 + ">" + object[p].answer1 + "</div>");
-      $(".questionaire").append(answer1);
-      var answer2 = $("<div><input type='radio' data-answer=false value=" + object[p].answer2 + ">" + object[p].answer2 + "</div>");
-      $(".questionaire").append(answer2);
-      var correctAnswer = $("<div><input type='radio' data-answer='true' value=" + object[p].correctAnswer + ">" + object[p].correctAnswer + "</div>");
-      $(".questionaire").append(correctAnswer);
-      var answer3 = $("<div><input type='radio' data-answer=false value=" + object[p].answer3 + ">" + object[p].answer3 + "</div>");
-      $(".questionaire").append(answer3);
+      var answer1 = $("<label><input type='radio' name='" + object[p].name + "' data-answer= false >" + object[p].answer1 + "</label>");
+      $(formGroup).append(answer1);
+      var answer2 = $("<label><input type='radio' name='" + object[p].name + "' data-answer= false >" + object[p].answer2 + "</label>");
+      $(formGroup).append(answer2);
+      var correctAnswer = $("<label><input type='radio' name='" + object[p].name + "' data-answer= true >" + object[p].correctAnswer + "</label>");
+      $(formGroup).append(correctAnswer);
+      var answer3 = $("<label><input type='radio' name='" + object[p].name + "' data-answer= false >" + object[p].answer3 + "</label>");
+      $(formGroup).append(answer3);
     }
   }
 }
+
+//Make sure only one button is clicked per group;
+
 
 $(".submit").click(function() {
   stop();
@@ -131,6 +153,7 @@ $(".submit").click(function() {
 function checkAnswers() {
   stop();
   $(".questionaire input[type=radio]:checked").each(function() {
+      console.log(this);
       var answer = $(this).attr("data-answer");
       if(answer === 'true') {
         right++;
@@ -138,12 +161,14 @@ function checkAnswers() {
         wrong++;
       }
   });
+    var score = ((right / 10) * 100);
   //return false;*/
     //Logic to delete the item
     $(".questionaire, .submit, #show-number, .instructions").css("display", "none");
     $(".answerHead, .results").css("display", "block");
     $(".right").text(right);
     $(".wrong").text(wrong);
+    $(".score").text("You got " + score + "%!");
 }
 
 //Load Triva Questions
